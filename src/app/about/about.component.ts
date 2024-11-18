3de58498f85fa905aba6f22dbd3951e5aa0217a4
nikita panwar <nikkipanwar11@gmail.com>
2024-11-18 16:21:52 +0530

    about done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -22,4 +22,26 @@ export class AboutComponent {
   setActiveTab(tab: number) {
     this.activeTab = tab;
   }
+
+  scrollPosition = 0;
+  itemWidth = 310; // Adjust based on your design
+
+  scrollNext() {
+    const container = document.querySelector('.item-container') as HTMLElement;
+    if (this.scrollPosition < container.scrollWidth - container.clientWidth) {
+      this.scrollPosition += this.itemWidth;
+      container.scrollTo({ left: this.scrollPosition, behavior: 'smooth' });
+    }
+  }
+
+  scrollPrev() {
+    const container = document.querySelector('.item-container') as HTMLElement;
+    if (this.scrollPosition > 0) {
+      this.scrollPosition -= this.itemWidth;
+      container.scrollTo({ left: this.scrollPosition, behavior: 'smooth' });
+    }
+  }
+
+  
+
 }
