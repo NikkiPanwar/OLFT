0ad247aaa7f75bef2f70d52f12e10d33ce2d6321
nikita panwar <nikkipanwar11@gmail.com>
2024-12-09 16:54:48 +0530

    complete

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-blog-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './blog-details.component.html',
   styleUrl: './blog-details.component.css'
 })
 export class BlogDetailsComponent {
+  card1 = [{
+    user: 'User',
+    comments: 1,
+    title: 'Hello World!',
+    link: 'package'
+  }];
+ 
+reviewData = {
+  name: '',
+  email: '',
+  message:'',
+  title: '',
+  autoSave:'false',
+};
 
+submitReview() {
+  console.log('Review submitted:', this.reviewData);
+  this.resetReviewForm();
+  this.toggleReviewForm();
+}
+
+resetReviewForm() {
+  this.reviewData = {
+    name: '',
+    email: '',
+    message:'',
+    title:'',
+    autoSave:'false',
+  };
+}
+isReviewFormVisible: boolean = false;
+
+toggleReviewForm() {
+  this.isReviewFormVisible = !this.isReviewFormVisible; 
+}
+
+onAutoSaveChange() {
+  if (this.reviewData.autoSave) {
+    this.autoSaveReviewData();
+  }
+}
+
+autoSaveReviewData() {
+  console.log('Auto-saving review data:', this.reviewData);
+}
 }
