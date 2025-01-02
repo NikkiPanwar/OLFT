5a8876ebf38cf11799d227ae4aeaac0e755008f3
Birobin Singh <erobinsingh578@gmail.com>
2025-01-02 13:34:59 +0530

    build generated

---
+++ dist/olft/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  AppServerModule
+} from "./chunk-XYEFBW6Y.mjs";
+import "./chunk-3AUX7KXG.mjs";
+import "./chunk-PTRYWQQD.mjs";
+export {
+  AppServerModule as default
+};
+//# sourceMappingURL=main.server.mjs.map
